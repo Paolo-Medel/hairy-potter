8f31cd380f7a1edacfc3b32207e5b7986e1a7213
Paolo Medel <paolomedel17@gmail.com>
2023-07-27 11:21:48 -0400

    finished project w passed tests

--- src/scripts/PotteryList.js
+++ src/scripts/PotteryList.js
@@ -1,16 +1,18 @@
-export const PotteryList = (usePotteryArray) => {
-  let htmlString = '<article class="whole_pottery_list">';
+import { usePottery } from "./PotteryCatalog.js";
+
+export const PotteryList = () => {
+  let htmlString = "";
+  let usePotteryArray = usePottery();
 
   for (const obj of usePotteryArray) {
-    htmlString += `<section class="pottery" id="pottery--1">
+    htmlString += `<section class="pottery" id="pottery--${obj.id}">
       <h2 class="pottery__shape">${obj.shape}</h2>
       <div class="pottery__properties">
         Item weighs ${obj.weight} grams and is ${obj.height} cm in height
       </div>
-      <div class="pottery__price">Price is ${obj.price}</div>
+      <div class="pottery__price">Price is $${obj.price}</div>
     </section>`;
   }
-  htmlString += `</article>`;
 
   return htmlString;
 };
